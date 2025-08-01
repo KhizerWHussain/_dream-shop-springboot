33a609949833bd661ba9b44bfccd3d425963229d
Khizer Hussain <khizwaseem@gmail.com>
2025-08-01 17:07:37 +0500

    feat: category controller work - 1

--- src/main/java/com/spring/practice/dreamshop/controllers/CategoryController.java
+++ src/main/java/com/spring/practice/dreamshop/controllers/CategoryController.java
@@ -1,8 +1,13 @@
 package com.spring.practice.dreamshop.controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.practice.dreamshop.model.Category;
+import com.spring.practice.dreamshop.response.APIResponse;
 import com.spring.practice.dreamshop.service.category.CategoryService;
 
 import lombok.RequiredArgsConstructor;
@@ -12,5 +17,10 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("${api.prefix}/categories")
 public class CategoryController {
     private final CategoryService categoryService;
+
+    public ResponseEntity<APIResponse> get() {
+        List<Category> categories = categoryService.get();
+        return ResponseEntity.ok(new APIResponse(true, "categories found successfully", categories));
+    }
 
 }
